9849f4bc95a7940f6ec2f1d7946986767985270e
Philippe Gadiou <philippe.gadiou@gmail.com>
2021-02-18 14:27:46 +0100

    initial commit

--- routes/companies.js
+++ routes/companies.js
@@ -1,7 +1,6 @@
 const express = require('express');
-const { PermissionMiddlewareCreator, RecordSerializer } = require('forest-express-sequelize');
+const { PermissionMiddlewareCreator } = require('forest-express-sequelize');
 const { companies } = require('../models');
-const superagent = require('superagent');
 
 const router = express.Router();
 const permissionMiddlewareCreator = new PermissionMiddlewareCreator('companies');
@@ -9,21 +8,6 @@ const permissionMiddlewareCreator = new PermissionMiddlewareCreator('companies')
 // This file contains the logic of every route in Forest Admin for the collection companies:
 // - Native routes are already generated but can be extended/overriden - Learn how to extend a route here: https://docs.forestadmin.com/documentation/v/v6/reference-guide/routes/extend-a-route
 // - Smart action routes will need to be added as you create new Smart Actions - Learn how to create a Smart Action here: https://docs.forestadmin.com/documentation/v/v6/reference-guide/actions/create-and-manage-smart-actions
-
-// Create a Company
-router.get('/companies/:recordId/relationships/pipedrivePersons', permissionMiddlewareCreator.list(), async (request, response, next) => {
-  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v6/reference-guide/routes/default-routes#create-a-record
-  const recordSerializer = new RecordSerializer({ name: 'pipedrivePersons' });
-  const parentId = request.params.recordId;
-  const limit = parseInt(request.query.page.size, 10) || 20;
-  const offset = (parseInt(request.query.page.number, 10) - 1) * limit;
-  const parent = await companies.findByPk(parentId);
-  return superagent
-    .get(`https://api.pipedrive.com/v1/organizations/${parent.pipedriveId}/persons?&api_token=${process.env.PIPEDRIVE_API_KEY}&limit=${limit}&start=${offset}`)
-    .then((apiResponse) => JSON.parse(apiResponse.res.text))
-    .then((rawRecords) => recordSerializer.serialize(rawRecords.data))
-    .then((records) => response.send(records));
-});
 
 // Create a Company
 router.post('/companies', permissionMiddlewareCreator.create(), (request, response, next) => {
